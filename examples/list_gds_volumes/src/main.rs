5cacc24ba26912adfeb198dda9bef4fbf4c02cc1
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2022-06-01 00:09:19 +1000

    From gds:// to S3 presigned URL is now working, needs proper error control/mapping and general cleanup though

--- examples/list_gds_volumes/src/main.rs
+++ examples/list_gds_volumes/src/main.rs
@@ -1,6 +1,7 @@
 use dirs::home_dir;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
+use std::io::Error;
 
 use ica_gds::apis::configuration::ApiKey;
 use ica_gds::apis::configuration::Configuration;
@@ -21,17 +22,17 @@ async fn main() {
 
 // TODO: Tidy up this messy function
 // TODO: Read from env vars first and then check filesystem, in this order
-async fn read_access_token() -> String {
+async fn read_access_token() -> Result<String, Error> {
     let f =
         File::open(home_dir().unwrap().to_str().unwrap().to_owned() + "/.ica/.session.aps2.yaml")
             .expect("Cannot open file");
     let ica: IcaConfig = serde_yaml::from_reader(f).expect("Could not read values.");
-    ica.access_token
+    Ok(ica.access_token)
 }
 
 async fn list_gds_volumes() {
     let mut conf = Configuration::default();
-    let key = read_access_token().await;
+    let key = read_access_token().await.unwrap();
     let apikey = ApiKey {
         prefix: Some("Bearer".to_string()),
         key,
