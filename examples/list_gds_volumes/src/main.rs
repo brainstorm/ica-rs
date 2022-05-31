bde2f8457c4e13bdf5664d31b16a2eb877167ded
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2022-05-31 17:21:09 +1000

    Success, need to adjust pagination and tenant_id retrieval accordingly

--- examples/list_gds_volumes/src/main.rs
+++ examples/list_gds_volumes/src/main.rs
@@ -1,12 +1,41 @@
+use dirs::home_dir;
+use std::fs::File;
+use serde::{Deserialize, Serialize};
+
 use ica_gds::apis::configuration::Configuration;
+use ica_gds::apis::configuration::ApiKey;
+use ica_gds::apis::volumes_api::list_volumes;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct IcaConfig {
+    #[serde(rename(deserialize = "access-token"))]
+    access_token: String,
+    #[serde(rename(deserialize = "session-token"))]
+    session_token: String
+}
 
 #[tokio::main]
 async fn main() {
     list_gds_volumes().await;
 }
 
+// TODO: Tidy up this messy function
+async fn read_access_token() -> String {
+    let f = File::open(home_dir().unwrap().to_str().unwrap().to_owned() + "/.ica/.session.aps2.yaml").expect("Cannot open file");
+    let ica: IcaConfig = serde_yaml::from_reader(f).expect("Could not read values.");
+    ica.access_token
+}
+
 async fn list_gds_volumes() {
-    let conf = Configuration::default();
-    let res = conf.client.get("http://httpbin.org/").send().await;
-    println!("{:#?}", res.unwrap());
+    let mut conf = Configuration::default();
+    let default_pagination = "eyJwYWdlU2l6ZSI6MTAsInBhZ2VTdGFydElkIjowLCJwYWdlRW5kSWQiOjEwfQ=="; // TODO: base64-encoded '{"pageSize":10,"pageStartId":0,"pageEndId":10}', build this dynamically
+    let tenant_id = "void"; // TODO: Obtain this programatically
+    let key = read_access_token().await;
+    let apikey = ApiKey{ prefix: Some("Bearer".to_string()), key };
+    conf.api_key = Some(apikey);
+
+    // page_size: Option<i32>, page_token: Option<&str'>, include: Option<&str>, tenant_id: Option<&str>, volume_configuration_name: Option<&str>) -> Result<crate::models::VolumeListResponse, Error<ListVolumesError>> {
+    let res = list_volumes(&conf, Some(10), Some(default_pagination), Some("ObjectStoreAccess"), Some(tenant_id), Some("vol_config_name")).await;
+    println!("{:#?}", res);
+
 }
